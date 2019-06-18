b4f5b3ebfeeec4679eed288f7e04f926cd06963e
mrd198 <50080807+mrd198@users.noreply.github.com>
2019-06-18 13:10:50 +0700

    bt

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
 from flask import Flask, render_template,request,redirect,url_for,session
 from app2 import get_all, insert_data_book
+from app3 import get_all2,insert_data_user
 app = Flask(__name__)
 app.secret_key = 'jjsdkfjkjdkfj515'
 
 
 @app.route('/', methods = ['POST'])
 def post_login():
-    tk = request.form.get('name')
-    mk = request.form.get('mk')
+    tk = request.form.get('namedn')
+    mk = request.form.get('mkdn')
     if tk == "admin" and mk == "admin":
-      session['name'] = request.form.get('name')
-      return redirect(url_for("index2"))
+      session['namedn'] = request.form.get('namedn')
+      return redirect(url_for("indexTC"))
     else:
       return "Login false"
 
-@app.route('/book', methods = ['POST'])
+@app.route('/AddDataBook', methods = ['POST'])
 def post():
+    book_mas = request.form.get('mas')
     book_name = request.form.get('name')
     book_price = int(request.form.get('price'))
-    insert_data_book(book_name,book_price)
-    return redirect(url_for("index2"))
+    book_nxb = request.form.get('nxb')
+    insert_data_book(book_mas,book_name,book_price,book_nxb)
+    return redirect(url_for("indexS"))
+  
+@app.route('/AddCardUser', methods = ['POST'])
+def posrUser():
+   user_ma = request.form.get('ma')
+   user_name = request.form.get('name')
+   user_phone = request.form.get('phone')
+   user_sex = request.form.get('sex')
+   user_add = request.form.get('add')
+   insert_data_user(user_ma,user_name,user_phone,user_sex,user_add)
+   return redirect(url_for('indexKH'))
 
-@app.route('/book')
-def index2():
-    if 'name' in session:
-        return render_template("index2.html", dulieu = get_all())
+@app.route('/AddCardUser')
+def indexKH():
+  if 'namedn' in session:
+        return render_template("indexKH.html", dulieu = get_all2())
+  else:
+        return redirect(url_for("index"))
+
+@app.route('/AddDataBook')
+def indexS():
+    if 'namedn' in session:
+        return render_template("indexS.html", dulieu = get_all())
     else:
         return redirect(url_for("index"))
+
+@app.route('/TrangChu')
+def indexTC():
+  if 'namedn' in session:
+      return render_template("indexTC.html", dulieu = get_all())
+  else:
+      return redirect(url_for("index"))
 
 @app.route('/')
 def index():
--- app3.py
+++ app3.py
@@ -3,13 +3,10 @@ import pymongo
 uri = "mongodb://mrd198:12051998d@ds335957.mlab.com:35957/c4e32thuvien"
 client = pymongo.MongoClient(uri)
 db = client.c4e32thuvien
-collection = db.sach
+collection = db.user
 
-def get_all():
+def get_all2():
     return list(collection.find())
 
-def insert_data_book(mas,name,price,nxb):
-    collection.insert_one({'mas':mas,'name':name,'price':price, 'nxb':nxb})
-
-    
-
+def insert_data_user(ma,name,phone,sex,add):
+    collection.insert_one({'ma':ma,'name':name,'phone':phone, 'sex':sex, 'add':add})
