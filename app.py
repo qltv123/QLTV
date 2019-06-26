b77fd5f822c12e3994245b75bb90a8819cc4882f
mrd198 <50080807+mrd198@users.noreply.github.com>
2019-06-26 12:48:49 +0700

    bt

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 app.secret_key = 'jjsdkfjkjdkfj515'
 
 
-@app.route('/', methods = ['POST'])
+@app.route('/DN', methods = ['POST'])
 def post_login():
     tk = request.form.get('namedn')
     mk = request.form.get('mkdn')
-    if tk == "admin" and mk == "admin":
+    if tk == "admin" or "ntduc" and mk == "admin" or "12051998":
       session['namedn'] = request.form.get('namedn')
       return redirect(url_for("indexTC"))
     else:
@@ -17,70 +17,75 @@ def post_login():
 
 @app.route('/AddBook', methods = ['POST'])
 def post():
-    book_name = request.form.get('names')
-    book_link = request.form.get('link')
-    book_sl = request.form.get('sl')
-    book_nxb = request.form.get('nxb')
-    book_price = int(request.form.get('price'))
-    insert_data_book(book_name,book_link,book_sl,book_nxb,book_price)
-    return redirect(url_for("indexAB"))
-  
+      book_name = request.form.get('names')
+      book_link = request.form.get('link')
+      book_sl = request.form.get('sl')
+      book_nxb = request.form.get('nxb')
+      book_price = int(request.form.get('price'))
+      if 'namedn' in session:
+            insert_data_book(book_name,book_link,book_sl,book_nxb,book_price)
+            return redirect(url_for("indexAB"))
+      else:
+            return "Yêu cầu đăng nhập để thực hiện chức năng này"
+
 @app.route('/AddCardUser', methods = ['POST'])
 def posrUser():
-   user_name = request.form.get('namek')
-   user_sex = request.form.get('sex')
-   user_bi = request.form.get('birth')
-   user_cmt = request.form.get('cmt')
-   user_job = request.form.get('job')
-   user_le = request.form.get('level')
-   user_wp = request.form.get('wp')
-   user_phone = request.form.get('phone')
-   user_ct = request.form.get('ct')
-   user_ci = request.form.get('city')
-   user_add = request.form.get('add')
-   user_ti = request.form.get('time')
-   insert_data_user(user_name,user_sex,user_bi,user_cmt,user_job,user_le,user_wp,user_phone,user_ct,user_ci,user_add,user_ti)
-   return redirect(url_for('indexKH'))
+      user_name = request.form.get('namek')
+      user_sex = request.form.get('sex')
+      user_bi = request.form.get('birth')
+      user_cmt = request.form.get('cmt')
+      user_job = request.form.get('job')
+      user_le = request.form.get('level')
+      user_wp = request.form.get('wp')
+      user_phone = request.form.get('phone')
+      user_ct = request.form.get('ct')
+      user_ci = request.form.get('city')
+      user_add = request.form.get('add')
+      user_ti = request.form.get('time')
+      if 'namedn' in session:
+            insert_data_user(user_name,user_sex,user_bi,user_cmt,user_job,user_le,user_wp,user_phone,user_ct,user_ci,user_add,user_ti)
+            return redirect(url_for('indexKH'))
+      else:
+            return "Yêu cầu đăng nhập để thực hiện chức năng này"
 
 @app.route('/edit_book/<book_id>',methods = ['POST'])
 def put_book(book_id):
     bo_names = request.form.get('names')
+    bo_li = request.form.get('link')
     bo_nxb = request.form.get('nxb')
     bo_sl = int(request.form.get('sl'))
     bo_price = int(request.form.get('price'))
-    update_book_by_id(book_id, bo_names, bo_nxb, bo_sl, bo_price)
+    update_book_by_id(book_id,bo_names, bo_li, bo_nxb, bo_sl, bo_price)
     return redirect(url_for('indexAB'))
  
 @app.route('/edit_book/<book_id>')
 def func_name(book_id):
-    bo = get_book_by_id(book_id) 
-    return render_template('edit_book.html',book = bo)
+      if 'namedn' in session:
+            bo = get_book_by_id(book_id) 
+            return render_template('edit_book.html',book = bo)
+      else:
+            return "Yêu cầu đăng nhập để thực hiện chức năng này"
 
 @app.route('/AddCardUser')
 def indexKH():
-  if 'namedn' in session:
-        return render_template("cate_list2.html", dulieu = get_all2())
-  else:
-        return redirect(url_for("index"))
+      return render_template("cate_list2.html")
+  
 
 @app.route('/TrangChu')
 def indexTC():
-  if 'namedn' in session:
-      return render_template("cate_list3.html", dulieu = get_all())
-  else:
-      return redirect(url_for("index"))
+      return render_template("cate_list3.html")
 
 @app.route('/AddBook')
-def indexAB():
-  if 'namedn' in session:
+def indexAB(): 
       return render_template("cate_list.html",dulieu = get_all())
-  else:
-      return redirect(url_for("index"))
 
 @app.route('/delete_book/<book_id>')
 def delete(book_id):
-   delete_data_book(book_id)
-   return redirect(url_for("indexAB"))
+      if 'namedn' in session:
+            delete_data_book(book_id)
+            return redirect(url_for("indexAB"))
+      else:
+            return "Yêu cầu đăng nhập để thực hiện chức năng này"
 
 @app.route('/NQ')
 def indexNQ():
@@ -96,14 +101,20 @@ def indexQD():
       else:
             return redirect(url_for("index"))
 
-@app.route('/')
+@app.route('/IFU')
+def indexIFU():
+   return render_template("cate_list6.html",dulieu2 = get_all2())
+
+@app.route('/DN')
 def index():
     return render_template("index.html")
 
 @app.route('/out')
 def method_name():
-   del session["namedn"]
-   return redirect(url_for("index"))
+   del session ['namedn']
+   return redirect(url_for("indexTC"))
+
+
 
 
 if __name__ == '__main__':
--- app2.py
+++ app2.py
@@ -6,22 +6,21 @@ client = pymongo.MongoClient(uri)
 db = client.c4e32thuvien
 collection = db.sach
 
+
 def get_all():
     return list(collection.find())
 
 def insert_data_book(names,link,sl,nxb,price):
-    collection.insert_one({'names':names,'link':link,'sl':sl,'nxb':nxb,'price':price})
+    collection.insert_one({"names":names,"link":link,"sl":sl,"nxb":nxb,"price":price})
 
-def update_book_by_id(book_id,names,nxb,sl,price): 
+def update_book_by_id(book_id,names,link,nxb,sl,price): 
     collection.update_one({"_id":ObjectId(book_id)},
-    {"$set":{'names':names,'nxb':nxb,'sl':sl,'price':price}})
+    {"$set":{"names":names,"link":link,"nxb":nxb,"sl":sl,"price":price}})
 
 def get_book_by_id(book_id): 
     return collection.find_one({"_id":  ObjectId(book_id)})
 
 def delete_data_book(book_id):
     collection.delete_one({"_id":  ObjectId(book_id)})
-
-
     
 
